e8b9b8e7cccf6533700ca65876d9d8ad07975ed7
nikak <nikakar@gmail.com>
2022-06-01 17:33:19 +0200

    using @JsonFormat

--- src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -22,6 +22,9 @@ class BeerDtoTest extends BaseTest {
         String json =
                 "{\"beerId\":\"bc7c07bb-a4fe-4abf-bc90-b3b25d1f2206\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":12344586,\"price\":12.99,\"createdDate\":\"2022-05-31T20:03:30.066722+02:00\",\"lastUpdatedDate\":\"2022-05-31T20:03:30.068245+02:00\"}";
         BeerDto dto = objectMapper.readValue(json, BeerDto.class);
+
+//        with jsonFormatted dto:
+//        {"beerName":"BeerName","beerStyle":"Ale","upc":12344586,"price":"12.99","createdDate":"2022-06-01T17:+0200","lastUpdatedDate":"2022-06-01T17:31:25.391265+02:00","beerId":"cd655525-b5f0-4252-a367-c5a01d0297f3"}
         System.out.println(dto);
     }
 }
